c097e40435775e40c7f8b5d4764fba4443fd8ddd
Gilbert Engalan <gilbertpude2003@gmail.com>
2025-08-21 22:41:38 +0800

    day1: setup folder structure

---
+++ backend/server/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import { connectDB } from '../config/db.js';
+
+dotenv.config()
+const PORT = process.env.PORT
+
+const app = express();
+
+//json parser middleware 
+app.use(express());
+
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port: ${PORT}`);
+      });
+}) 
